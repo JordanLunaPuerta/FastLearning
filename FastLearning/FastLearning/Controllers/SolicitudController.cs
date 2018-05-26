3716ae99026cae4ef910724eb5a2374f03abed17
JordanLuna <1412931@utp.edu.pe>
2018-05-26 01:02:15 -0500

    FastLearning
    v5

---
+++ FastLearning/FastLearning/Controllers/SolicitudController.cs
@@ -0,0 +1,216 @@
+﻿using FastLearning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FastLearning.Controllers
+{
+    public class SolicitudController : Controller
+    {
+        FastLearningDBEntities db = new FastLearningDBEntities();
+
+        public ActionResult Index(string buscar)
+        {
+            int id_usuario = Convert.ToInt32(Session["id"]);
+            Session.Contents.Remove("solicitud");
+            Session.Contents.Remove("errorsolicitud");
+            List<Solicitud> solicitudes = db.Solicitud.Where(s=>s.estado==1 && s.id_usuario!=id_usuario).ToList();
+            if (!String.IsNullOrEmpty(buscar))
+            {
+                solicitudes = solicitudes.Where(c => c.nombre.Contains(buscar)).ToList();
+            }
+            return View(solicitudes.OrderByDescending(s => s.fecha_registro));
+        }
+
+        public ActionResult ListarSolicitudPendientes(string buscar)
+        {
+            List<Solicitud> solicitudes = db.Solicitud.Where(s => s.estado == 1).ToList();
+            if (!String.IsNullOrEmpty(buscar))
+            {
+                solicitudes = solicitudes.Where(c => c.nombre.Contains(buscar)).ToList();
+            }
+            return View(solicitudes.OrderByDescending(s=>s.fecha_registro));
+        }
+
+        public ActionResult ListarSolicitudAprobadas(string buscar)
+        {
+            List<Solicitud> solicitudes = db.Solicitud.Where(s => s.estado == 2).ToList();
+            if (!String.IsNullOrEmpty(buscar))
+            {
+                solicitudes = solicitudes.Where(c => c.nombre.Contains(buscar)).ToList();
+            }
+            return View(solicitudes.OrderByDescending(s => s.fecha_registro));
+        }
+
+        public ActionResult AprobarSolicitud(int id)
+        {
+            Solicitud solicitud = db.Solicitud.Find(id);
+            solicitud.estado = 2;
+            db.SaveChanges();
+            return RedirectToAction("ListarSolicitudPendientes","Solicitud");
+        }
+
+        //public ActionResult CancelarAprobacion(int id)
+        //{
+        //    Solicitud solicitud = db.Solicitud.Find(id);
+        //    solicitud.estado = 1;
+        //    db.SaveChanges();
+        //    return RedirectToAction("ListarSolicitudAprobadas", "Solicitud");
+        //}
+      
+        public ActionResult ListarSolicitudPorUsuario()
+        {
+            int id_usuario = Convert.ToInt32(Session["id"]);
+            Usuario usuario = db.Usuario.Find(id_usuario);
+            return View(db.Solicitud.Where(s=>s.id_usuario==id_usuario).ToList().OrderByDescending(s => s.fecha_registro));
+        }
+
+        public ActionResult ListarSolicitudPorVotacion()
+        {
+            int id_usuario = Convert.ToInt32(Session["id"]);
+            Usuario usuario = db.Usuario.Find(id_usuario);
+            return View(db.SolicitudUsuario.Where(su=>su.id_usuario==id_usuario).ToList().OrderByDescending(s => s.fecha_registro));
+        }
+
+        public ActionResult EliminarVoto(int id)
+        {
+            int id_usuario = Convert.ToInt32(Session["id"]);
+            Solicitud solicitud = db.Solicitud.Find(id);
+            SolicitudUsuario su = db.SolicitudUsuario.Find(solicitud.id,id_usuario);
+            db.SolicitudUsuario.Remove(su);
+            db.SaveChanges();
+            return RedirectToAction("ListarSolicitudPorVotacion","Solicitud");
+        }
+
+        public ActionResult ListarVotantesPorSolicitud(int id)
+        {          
+            return View(db.SolicitudUsuario.Where(su=>su.id_solicitud==id).ToList());
+        }
+
+        public ActionResult ListarVotantesPorSolicitudVotada(int id)
+        {
+            return View(db.SolicitudUsuario.Where(su => su.id_solicitud == id).ToList());
+        }
+
+        public ActionResult ListarVotantesPorSolicitudAdmin(int id)
+        {
+            return View(db.SolicitudUsuario.Where(su => su.id_solicitud == id).ToList());
+        }
+
+        public ActionResult PartialListarVotantesPorSolicitud()
+        {
+            int id = Convert.ToInt32(Session["solicitud"]);
+            return PartialView(db.SolicitudUsuario.Where(su => su.id_solicitud == id).ToList());
+        }
+
+        public ActionResult VotarPorSolicitud(int id)
+        {
+            Solicitud solicitud= db.Solicitud.Find(id);
+            Session["solicitud"] = solicitud.id;
+            ViewBag.ErrorSolicitud = Session["errorsolicitud"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult VotarPorSolicitud(SolicitudUsuario su)
+        {
+            Session["errorsolicitud"] = "";
+            int id_usuario = Convert.ToInt32(Session["id"]);
+            int id_solicitud = Convert.ToInt32(Session["solicitud"]);
+            try
+            {
+                if (db.SolicitudUsuario.Find(id_solicitud,id_usuario)== null)
+                {
+                    Session["errorsolicitud"] = "";
+                    su.id_solicitud = id_solicitud;
+                    su.id_usuario = id_usuario;
+                    su.fecha_registro = DateTime.Now;
+                    db.SolicitudUsuario.Add(su);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    Session["errorsolicitud"] = "Usted ya votó por esta solicitud";
+                    return RedirectToAction("VotarPorSolicitud","Solicitud", new { id=id_solicitud});
+                }               
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Solicitud solicitud)
+        {
+            int id = Convert.ToInt32(Session["id"]);
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                solicitud.id_usuario = id;
+                solicitud.fecha_registro = DateTime.Now;
+                solicitud.estado = 1;
+                db.Solicitud.Add(solicitud);
+                db.SaveChanges();
+
+                return RedirectToAction("ListarSolicitudPorUsuario");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Solicitud solicitud = db.Solicitud.Find(id);
+            return View(solicitud);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Solicitud solicitud)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                Solicitud s = db.Solicitud.Find(solicitud.id);
+                s.nombre = solicitud.nombre;
+                s.descripcion = solicitud.descripcion;
+                db.SaveChanges();
+                return RedirectToAction("ListarSolicitudPorUsuario");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            Solicitud solicitud = db.Solicitud.Find(id);
+            db.Solicitud.Remove(solicitud);
+            db.SaveChanges();
+            return RedirectToAction("ListarSolicitudPorUsuario");
+        }
+
+        public ActionResult EliminarPendiente(int id)
+        {
+            Solicitud solicitud = db.Solicitud.Find(id);
+            db.Solicitud.Remove(solicitud);
+            db.SaveChanges();
+            return RedirectToAction("ListarSolicitudPendientes");
+        }
+
+    }
+}
---
+++ FastLearning/FastLearning/Controllers/UsuarioController.cs
@@ -0,0 +1,161 @@
+﻿using FastLearning.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace FastLearning.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        FastLearningDBEntities db = new FastLearningDBEntities();
+
+        // GET: Usuario
+        public ActionResult Index(string currentFilter,string buscar, int? page)
+        {
+            if (buscar !=null)
+            {
+                page = 1;
+            }
+            else
+            {
+                buscar = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = buscar;
+
+            List<Usuario> usuarios = db.Usuario.Where(u=>u.rol==1).ToList();
+            if (!String.IsNullOrEmpty(buscar))
+            {
+                usuarios = usuarios.Where(u=>u.nombres.Contains(buscar) || u.apellidos.Contains(buscar)).ToList();
+            }
+
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+
+            return View(usuarios.ToPagedList(pageNumber,pageSize));
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Detalles(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            return View(usuario);
+        }
+
+        public ActionResult Perfil()
+        {
+            int id = Convert.ToInt32(Session["id"]);
+            Usuario usuario = db.Usuario.Find(id);
+            return View(usuario);
+        }
+
+        public ActionResult EditarPerfil(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            return View(usuario);
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult EditarPerfil(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                Usuario u = db.Usuario.Find(usuario.id);
+                u.nombres = usuario.nombres;
+                u.apellidos = usuario.apellidos;
+                u.telefono = usuario.telefono;
+                u.email = usuario.email;
+                u.contrasena = usuario.contrasena;
+                u.confirmar_contrasena = usuario.confirmar_contrasena;
+                db.SaveChanges();
+                return RedirectToAction("Perfil");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Usuario/Create
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Crear(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            if (db.Usuario.Any(u => u.email == usuario.email))
+            {
+                ViewBag.Duplicado = "Usuario ya existe";
+                return View("RegistrarUsuario", usuario);
+            }
+            try
+            {
+                usuario.fecha_registro = DateTime.Now;
+                usuario.calificacion_alumno = 5;
+                usuario.calificacion_profesor = 5;
+                usuario.rol = 1;
+                usuario.puntos = 0;
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Editar(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            return View(usuario);
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Editar(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                Usuario u = db.Usuario.Find(usuario.id);
+                u.nombres = usuario.nombres;
+                u.apellidos = usuario.apellidos;
+                u.telefono = usuario.telefono;
+                u.email = usuario.email;
+                u.contrasena = usuario.contrasena;
+                u.confirmar_contrasena = usuario.confirmar_contrasena;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
